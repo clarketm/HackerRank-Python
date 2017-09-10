48edcbebd65502dfc9b52ca0102750e7517bc26b
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-09 18:29:25 -0700

    add print solution

---
+++ intro/print.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Print Function
+
+
+def print_range(n):
+    # print [1, n]
+    for i in range(1, n + 1):
+        print(i, end='')
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_range(n)
