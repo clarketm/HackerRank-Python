87eaa20c5378a1776f3dd5f0c212175165de319c
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-11 12:57:57 -0700

    add general regex solution(s)

--- regex/regex.py
+++ regex/regex.py
@@ -1,33 +1,62 @@
 #!/usr/bin/env python3
 
 
-# Lists
-# https://www.hackerrank.com/challenges/python-lists/problem
+import re
 
-def main(N):
-    list = []
 
-    while N > 0:
-        i = input().split()
+# Matching Specific String
+# https://www.hackerrank.com/challenges/matching-specific-string/problem
+def match_specific_string(string):
+    pattern = r'hackerrank'
+    match = re.findall(pattern, string)
+    print("Number of matches :", len(match))
 
-        if i[0] == 'insert':
-            list.insert(int(i[1]), int(i[2]))
-        elif i[0] == 'remove':
-            list.remove(int(i[1]))
-        elif i[0] == 'append':
-            list.append(int(i[1]))
-        elif i[0] == 'sort':
-            list.sort()
-        elif i[0] == 'reverse':
-            list.reverse()
-        elif i[0] == 'pop':
-            list.pop()
-        elif i[0] == 'print':
-            print(list)
 
-        N -= 1
+# Matching Anything But a Newline
+# https://www.hackerrank.com/challenges/matching-anything-but-new-line/problem
+def match_anything_but_newline(string):
+    pattern = r'^(.{3}\.){3}.{3}$'
+    match = re.match(pattern, string)
+    print(str(bool(match)).lower())
+
+
+# Matching Digits & Non-Digit Characters
+# https://www.hackerrank.com/challenges/matching-digits-non-digit-character/problem
+def match_digits_and_nondigits(string):
+    pattern = r'(\d{2}\D){2}\d{4}'
+    match = re.search(pattern, string)
+    print(str(bool(match)).lower())
+
+
+# Matching Whitespace & Non-Whitespace Character
+# https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character/problem
+def match_whitespace_and_nonwhitespace(string):
+    pattern = r'(\S{2}\s){2}\S{2}'
+    match = re.search(pattern, string)
+    print(str(bool(match)).lower())
+
+
+# Matching Word & Non-Word Character
+# https://www.hackerrank.com/challenges/matching-word-non-word/problem
+def match_word_and_nonword(string):
+    pattern = r'\w{3}\W\w{10}\W\w{3}'
+    match = re.search(pattern, string)
+    print(str(bool(match)).lower())
+
+
+# Matching Start & End
+# https://www.hackerrank.com/challenges/matching-start-end/problem
+def match_start_and_end(string):
+    pattern = r'^\d\w{4}\.$'
+    match = re.search(pattern, string)
+    print(str(bool(match)).lower())
 
 
 if __name__ == '__main__':
-    N = int(input())
-    main(N)
+    string = input()
+    # match_specific_string(string)
+    # match_anything_but_newline(string)
+    # match_digits_and_nondigits(string)
+    # match_whitespace_and_nonwhitespace(string)
+    # match_word_and_nonword(string)
+    # match_start_and_end(string)
