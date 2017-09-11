3750c30c8f77b37412e92b4e72e6374a9bc591ca
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-11 12:58:22 -0700

    add stack_exchange_scraper.py solution

---
+++ regex/stack_exchange_scraper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import re
+import sys
+
+
+# Build a Stack Exchange Scraper
+# https://www.hackerrank.com/challenges/stack-exchange-scraper/problem
+def stack_exchange_scraper(html):
+    pattern = r'"question-summary-(\d+)".*?"question-hyperlink">(.*?)<.*?"relativetime">(.*?)<'
+    matches = re.findall(pattern, html, re.DOTALL)
+    for match in matches:
+        print(';'.join(match))
+
+
+if __name__ == '__main__':
+    html = sys.stdin.read()
+    stack_exchange_scraper(html)
