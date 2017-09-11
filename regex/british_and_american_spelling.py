d0149768d28f43e5e9cc526a4ef2a64698802673
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-11 15:35:22 -0700

    add british_and_american_spelling.py (part 2) solution

--- regex/british_and_american_spelling.py
+++ regex/british_and_american_spelling.py
@@ -5,9 +5,22 @@ import re
 
 # British and American Style of Spelling
 # https://www.hackerrank.com/challenges/uk-and-us/problem
-def british_english_diff_count(text, tests):
+def british_english_diff_count_1(text, tests):
     for test in tests:
-        pattern = r'{}[sz]e'.format(test[:-2])
+        pattern = r'\b{}[sz]e\b'.format(test[:-2])
+        matches = re.findall(pattern, text)
+        print(len(matches))
+
+
+# UK and US: Part 2
+# https://www.hackerrank.com/challenges/uk-and-us-2/problem
+def british_english_diff_count_2(text, tests):
+    seq = 'our'
+
+    for test in tests:
+        start = test.index(seq)
+        end = start + len(seq)
+        pattern = r'\b{}(or|our){}\b'.format(test[:start], test[end:])
         matches = re.findall(pattern, text)
         print(len(matches))
 
@@ -23,4 +36,5 @@ if __name__ == '__main__':
     for _ in range(t):
         tests.append(input())
 
-    british_english_diff_count('\n'.join(lines), tests)
+    british_english_diff_count_1('\n'.join(lines), tests)
+    british_english_diff_count_2('\n'.join(lines), tests)
