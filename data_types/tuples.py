03ae684e92c281476aef349557fde91cf60552c1
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-12 22:57:14 -0500

    add tuples.py solution

---
+++ data_types/tuples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+# Tuples
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+
+def main(ints):
+    _hash = hash(tuple(ints))
+    print(_hash)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    ints = map(int, input().split())
+    main(ints)
