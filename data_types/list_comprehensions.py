1e5dd8e12dc2a44a109ba3d8e4f42b22fea6ae80
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-12 23:09:14 -0500

    add list_comprehensions.py solution

---
+++ data_types/list_comprehensions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+# List Comprehensions
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+
+def main(x, y, z, n):
+    result = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (i + j + k) != n]
+    print(result)
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    main(x, y, z, n)
