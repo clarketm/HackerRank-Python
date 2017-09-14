04c73daf4e6093bb53f03cb45cbacc144c63cd8b
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-13 22:24:56 -0500

    add polar_coordinates.py solution

---
+++ math/polar_coordinates.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Polar Coordinates
+# https://www.hackerrank.com/challenges/polar-coordinates/problem
+
+from cmath import phase, polar
+
+if __name__ == '__main__':
+    complex_number = complex(input())
+
+    print(abs(complex_number))
+    print(phase(complex_number))
+
+    # print(*polar(complex_number), sep='\n')
