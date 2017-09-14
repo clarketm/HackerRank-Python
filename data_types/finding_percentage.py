3489103ae7053add5badf11871f497c81856192b
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-13 19:49:13 -0500

    add finding_percentage.py solution

---
+++ data_types/finding_percentage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+# Finding the percentage
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    N = int(input())
+    student_average_map = {}
+
+    for _ in range(N):
+        student, grades = input().split(' ', 1)
+        grades = list(map(float, grades.split()))
+        student_average_map[student] = sum(grades) / len(grades)
+
+    print("{0:.2f}".format(student_average_map[input()], 2))
