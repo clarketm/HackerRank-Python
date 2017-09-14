0b3c1358cb77508b0b596154da92c74ecb3e62e6
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-13 21:30:06 -0500

    add itertools_combinations_with_replacement.py solution

---
+++ data_types/itertools_combinations_with_replacement.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+# itertools.combinations_with_replacement()
+# https://www.hackerrank.com/challenges/itertools-combinations-with-replacement/problem
+
+
+from itertools import combinations_with_replacement
+
+
+def get_combinations(string, max_chars):
+    combos = sorted(map(''.join, map(sorted, combinations_with_replacement(string, max_chars))))
+    print('\n'.join(combos))
+
+
+if __name__ == '__main__':
+    string, max_chars = input().split()
+    get_combinations(string, int(max_chars))
