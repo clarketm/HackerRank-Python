8d5eff254643bc4e8626c5c806b16dcecf7b4fa1
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-16 21:49:40 -0700

    add intro_to_sets.py solution

---
+++ sets/intro_to_sets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Introduction to Sets
+# https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+
+
+def average(heights):
+    heights = set(heights)
+    return sum(heights) / len(heights)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    heights = list(map(int, input().split()))
+    print(average(heights))
