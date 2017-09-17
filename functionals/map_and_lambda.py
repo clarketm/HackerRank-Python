d53211d20a4a2f68906bc4cd32f42c84768d211f
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-16 21:18:48 -0700

    add map_and_lambda.py solution

---
+++ functionals/map_and_lambda.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Map and Lambda Function
+# https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
+
+cube = lambda x: x ** 3
+
+
+def fibonacci(n):
+    seq = [0, 1]
+
+    for i in range(2, n):
+        seq.append(seq[i - 2] + seq[i - 1])
+
+    return [cube(i) for i in seq]
+
+
+# def fibonacci(n):
+#     seq = []
+
+#     for i in range(n):
+#         if i <= 1:
+#             seq.append(i)
+#         else:
+#             seq.append(seq[i - 2] + seq[i - 1])
+
+#     return list(map(cube, seq))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(fibonacci(n))
