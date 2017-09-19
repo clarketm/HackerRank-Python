d0a61b016cf0184d189afb0c16f31794193b8fd7
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-19 14:19:57 -0700

    add validate_email.py solution

---
+++ functionals/validate_email.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Validating Email Addresses With a Filter
+# https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
+
+import re
+
+
+def fun(s):
+    # It must have the username@websitename.extension format type.
+    # The username can only contain letters, digits, dashes and underscores.
+    # The website name can only have letters and digits.
+    # The maximum length of the extension is 3.
+    try:
+        return bool(re.match(r'^([\w-]+)@([a-zA-Z\d]+)\.(.{,3})$', s).groups())
+    except:
+        return False
+
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+    filtered_emails = filter_mail(emails)
+    filtered_emails.sort()
+    print(filtered_emails)
