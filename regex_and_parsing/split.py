fda1e81c5ca3e3127ebdb06ff235041298055ad6
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-19 13:49:54 -0700

    add split.py solution

---
+++ regex_and_parsing/split.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Re.split()
+# https://www.hackerrank.com/challenges/re-split/problem
+
+import re
+
+if __name__ == '__main__':
+    num = input()
+    segments = filter(None, re.split(r'[.,]', num))
+    print(*segments, sep='\n')
