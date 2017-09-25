08d0654e17e7a39d07a4a957a3262f645171f6fe
Travis Clarke <travis.m.clarke@gmail.com>
2017-09-24 17:17:08 -0700

    add named_tuple.py solution

---
+++ collections/named_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+
+if __name__ == '__main__':
+    n = int(input())
+    Student = namedtuple('Student', input().split())
+
+    # LIST COMPREHENSIONS
+    average = sum([int(Student(*input().split()).MARKS) for student in range(n)]) / n
+    print("{:.2f}".format(average))
+
+    # FOR LOOP
+    # total = 0
+    # for _ in range(n):
+    #     student = Student(*input().split())
+    #     total += int(student.MARKS)
+    #
+    # print("{:.2f}".format(total / n))
