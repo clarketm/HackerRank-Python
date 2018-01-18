93fc217ed0c45fdcc22735efde4e8d7f5f70d090
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-17 17:03:35 -0800

    add mutations.py solution

---
+++ strings/mutations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def mutate_string(string, position, character):
+    # Using list
+    # s_list = list(string)
+    # s_list[position] = character
+    # return ''.join(s_list)
+
+    # Using slices
+    return string[:position] + c + string[position + 1:]
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
