407354ca8d831ef4f12ec32053999a439e1ee466
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-18 10:16:05 -0800

    add find_a_string.py solution

---
+++ strings/find_a_string.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Find a String
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+
+def count_substring(string, substring):
+    length = len(substring)
+    i, count = 0, 0
+
+    # Using `while`
+    # while i < len(string):
+    #     if substring == string[i:i + length]:
+    #         count += 1
+    #     i += 1
+
+    # Using `while` optimized
+    while i < len(string) - (length - 1):
+        if substring == string[i:i + length]:
+            count += 1
+        i += 1
+
+    # Using `for` w/ `enumerate`
+    # for i, c in enumerate(string):
+    #     if substring == string[i:i + length]:
+    #         count += 1
+
+    # Using `for` w/ `range`
+    # for i in range(len(string)):
+    #     if substring == string[i:i + length]:
+    #         count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    string = input()
+    substring = input()
+    print(count_substring(string, substring))
