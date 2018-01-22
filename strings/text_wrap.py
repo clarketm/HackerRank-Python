e008e69f853dffafb3e0650b1218d607fc50a254
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-22 11:48:11 -0800

    add text_wrap.py solution

---
+++ strings/text_wrap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Text Wrap
+# https://www.hackerrank.com/challenges/text-wrap/problem
+
+
+import textwrap
+
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+
+if __name__ == '__main__':
+    string = input()
+    max_width = int(input())
+    print(wrap(string, max_width))
