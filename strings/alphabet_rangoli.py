7d0eeb822b539b0afae3d200c93c978d282620a2
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-22 13:55:58 -0800

    add alphabet_rangoli.py solution

---
+++ strings/alphabet_rangoli.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Alphabet Rangoli
+# https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+
+
+# Using a `terse` approach
+# def print_rangoli(size):
+#     import string
+#     alpha = string.ascii_lowercase
+#     l = []
+#     for i in range(size):
+#         s = "-".join(alpha[i:size])
+#         l.append((s[::-1] + s[1:]).center(4 * size - 3, "-"))
+#
+#     print('\n'.join(l[::-1] + l[1:]))
+
+
+# Using a `verbose` approach
+def print_rangoli(size):
+    start = ord('a')
+    end = start + (size - 1)
+
+    # width = ((size * 2) - 1) + ((size - 1) * 2)
+    # ...simplified
+    width = size * 4 - 3
+
+    for i in range(size):
+        chars = [chr(c) for c in range(end, start - 1, -1)]
+        l = chars[:i] + [chars[i]] + list(reversed(chars[:i]))
+        print('-'.join(l).center(width, '-'))
+
+    for i in reversed(range(size - 1)):
+        chars = [chr(c) for c in range(end, start - 1, -1)]
+        l = chars[:i] + [chars[i]] + list(reversed(chars[:i]))
+        print('-'.join(l).center(width, '-'))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
