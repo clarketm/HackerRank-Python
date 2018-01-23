7cb46a9ce2773ab530942b32732d2e8715ffd826
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-22 19:24:35 -0800

    update company_logo.py solution

--- collections/company_logo.py
+++ collections/company_logo.py
@@ -3,10 +3,17 @@
 # Company Logo
 # https://www.hackerrank.com/challenges/most-commons/problem
 
+
 from collections import Counter
+
+# def sort_comparator(item):
+#     letter, count = item
+#     return -count, letter
+
 
 if __name__ == "__main__":
     s = input().strip()
+    # letter_counts = sorted(Counter(s).items(), key=sort_comparator)
     letter_counts = sorted(Counter(s).items(), key=lambda item: (-item[1], item[0]))
 
     for letter, count in letter_counts[:3]:
