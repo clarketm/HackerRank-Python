c05001dcd3d1f33c5ee1ed56df62581ee01b9916
Travis Clarke <travis.m.clarke@gmail.com>
2018-01-23 10:10:29 -0800

    add iterables_and_iterators.py solution

---
+++ itertools/iterables_and_iterators.py
@@ -0,0 +1,22 @@
+#!/usr/bin.env python3
+
+# Iterables and Iterators
+# https://www.hackerrank.com/challenges/iterables-and-iterators/problem
+
+
+from itertools import combinations
+
+if __name__ == '__main__':
+    N = int(input())
+    letters = input().split()
+    K = int(input())
+
+    combs = list(combinations(letters, K))
+
+    # Using `filter`
+    with_a = list(filter(lambda i: 'a' in i, combs))
+
+    # Using `list comprehension`
+    # with_a = [i for i in combs if 'a' in i]
+
+    print('{:.3f}'.format(len(with_a) / len(combs)))
