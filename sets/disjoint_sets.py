334ee5219d11662b51545301fe3464afba27e464
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-05 14:45:13 -0800

    add disjoint_sets.py solution

---
+++ sets/disjoint_sets.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# No Idea!
+# https://www.hackerrank.com/challenges/no-idea/problem
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+
+    array = list(map(int, input().split()))
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
+
+    # Using an `iterative` approach
+    # happiness = 0
+    # for i in A:
+    #     if i in array:
+    #         happiness += 1
+    #
+    # for i in B:
+    #     if i in array:
+    #         happiness -= 1
+    #
+    # print(happiness)
+
+    # Using an `list comprehension` approach
+    pos_list = [int(i in A) - int(i in B) for i in array]
+    print(sum(pos_list))
