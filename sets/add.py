258408c7cdcd817c2a51c1144367bebb942e9347
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-05 14:56:36 -0800

    add add.py solution

---
+++ sets/add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Set .add()
+# https://www.hackerrank.com/challenges/py-set-add/problem
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+    # Using `set comprehension`
+    countries = {input() for item in range(N)}
+
+    # Using `iterative w/ .add()`
+    # countries = set()
+    # for _ in range(N):
+    #     countries.add(input())
+
+    print(len(countries))
