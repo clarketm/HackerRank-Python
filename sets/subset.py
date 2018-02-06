724ea152ff44e2142ccbaa14b5fbb9419aeaa8f9
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-06 11:53:00 -0800

    add subset.py solution

---
+++ sets/subset.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Check Subset
+# https://www.hackerrank.com/challenges/py-check-subset/problem
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        a = int(input())
+        A = set(input().split())
+        b = int(input())
+        B = set(input().split())
+        print(A.issubset(B))
