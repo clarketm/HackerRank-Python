87fc4dd8543f6e68ff712faa99af73467ef83b0b
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-06 10:25:27 -0800

    add mutations.py solution

---
+++ sets/mutations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Set Mutations
+# https://www.hackerrank.com/challenges/py-set-mutations/problem
+
+
+if __name__ == '__main__':
+    A = int(input())
+    s0 = {int(i) for i in input().split()}
+    N = int(input())
+
+    for _ in range(N):
+        method, _ = input().split()
+        s1 = {int(i) for i in input().split()}
+        getattr(s0, method)(s1)
+
+    print(sum(s0))
