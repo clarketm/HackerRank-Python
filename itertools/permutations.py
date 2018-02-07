35b04df63e0e7b66f200d9870bc774643c0f188c
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 11:29:43 -0800

    add permutations.py solution

---
+++ itertools/permutations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# itertools.permutations()
+# https://www.hackerrank.com/challenges/itertools-permutations/problem
+
+
+from itertools import permutations
+
+if __name__ == '__main__':
+    S, k = input().split()
+
+    print(*sorted([''.join(p) for p in permutations(S, int(k))]), sep='\n')
