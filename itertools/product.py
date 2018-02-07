f9e1d923d4a0114e71e29c516df9ac042a05f97b
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 11:20:45 -0800

    add product.py solution

---
+++ itertools/product.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# itertools.product()
+# https://www.hackerrank.com/challenges/itertools-product/problem
+
+from itertools import product
+
+if __name__ == '__main__':
+    A = map(int, input().split())
+    B = map(int, input().split())
+
+    print(*product(A, B))
