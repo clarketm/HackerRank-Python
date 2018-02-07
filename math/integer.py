a6d80d098ecdcaf2fc107ce5bbceb57e28216e59
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 10:18:33 -0800

    add integer.py solution

---
+++ math/integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Integers Come In All Sizes
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    print(a ** b + c ** d)
