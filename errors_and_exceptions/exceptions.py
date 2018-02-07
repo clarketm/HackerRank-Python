c837c9922f17d7956207f293fd69b4398dbc2985
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 11:36:12 -0800

    add exceptions.py solution

---
+++ errors_and_exceptions/exceptions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Exceptions
+# https://www.hackerrank.com/challenges/exceptions/problem
+
+
+if __name__ == '__main__':
+    T = int(input())
+
+    for _ in range(T):
+        try:
+            numerator, denominator = map(int, input().split())
+            print(numerator // denominator)
+        except (ValueError, ZeroDivisionError) as e:
+            print('Error Code:', e)
