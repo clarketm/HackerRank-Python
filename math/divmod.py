af0eb6fdf8873e2d6e66adbd3a2c87e6bdcec35d
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-06 16:41:07 -0800

    add divmod.py solution

---
+++ math/divmod.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Mod Divmod
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+
+if __name__ == '__main__':
+    numerator = int(input())
+    denominator = int(input())
+
+    # quotient
+    print(numerator // denominator)
+
+    # remainder
+    print(numerator % denominator)
+
+    # quotient and remainder
+    print(divmod(numerator, denominator))
