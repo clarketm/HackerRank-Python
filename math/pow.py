29021608214c2f188b490c69e896e96ce2f18463
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-06 16:47:01 -0800

    add pow.py solution

---
+++ math/pow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Power - Mod Power
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+
+    # a^b
+    # print(a ** b)
+    print(pow(a, b))
+
+    # a^b mod m
+    # print(a ** b % m)
+    print(pow(a, b, m))
