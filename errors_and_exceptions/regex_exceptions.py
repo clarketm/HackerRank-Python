8ba0bbddda707a841126fbd7b2c1b6c6ed7b8841
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 11:42:17 -0800

    add regex_exceptions.py solution

---
+++ errors_and_exceptions/regex_exceptions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Incorrect Regex
+# https://www.hackerrank.com/challenges/incorrect-regex/problem
+
+
+import re
+
+if __name__ == '__main__':
+    T = int(input())
+
+    for _ in range(T):
+        try:
+            pattern = input()
+            re.compile(pattern)
+            print(True)
+        except:
+            print(False)
