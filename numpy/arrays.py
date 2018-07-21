88117b000fd39a3dc77e822cb6237fb41bac3825
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-20 21:46:06 -0700

    update numpy -> arrays solution

--- numpy/arrays.py
+++ numpy/arrays.py
@@ -5,6 +5,12 @@
 
 import numpy
 
+
+def arrays(arr):
+    return numpy.array(arr[::-1], float)
+
+
 if __name__ == '__main__':
-    arr = input().split()
-    print(numpy.array(arr[::-1], float))
+    arr = input().strip().split(' ')
+    result = arrays(arr)
+    print(result)
