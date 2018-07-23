393cbf22b48d6fbacdb641382df05788a82f2d31
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 15:22:45 -0700

    add regex -> start_and_end solution

---
+++ regex_and_parsing/start_and_end.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Re.start() & Re.end()
+# https://www.hackerrank.com/challenges/re-start-re-end/problem
+
+import re
+
+if __name__ == '__main__':
+    S = input()
+    k = input()
+
+    p = re.compile(k)
+    m = p.search(S)
+
+    if not m:
+        print((-1, -1))
+
+    while m:
+        start = m.start()
+        end = m.end() - 1
+        print((start, end))
+        m = p.search(S, start + 1)
