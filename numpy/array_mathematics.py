6295732fc086bb4caa8e557be3be5eefabfb4d20
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 13:53:59 -0700

    add numpy -> array_mathematics solution

---
+++ numpy/array_mathematics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Array Mathematics
+# https://www.hackerrank.com/challenges/np-array-mathematics/problem
+
+import numpy
+
+if __name__ == '__main__':
+    N, M = list(map(int, input().split()))
+
+    a = numpy.array([input().split() for _ in range(N)], dtype=int)
+    b = numpy.array([input().split() for _ in range(N)], dtype=int)
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a // b)
+    print(a % b)
+    print(a ** b)
