e00507974f064573c3e255e3d8dc7d0ae0bbf306
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 14:37:28 -0700

    add numpy -> inner_and_outer solution

---
+++ numpy/inner_and_outer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Inner and Outer
+# https://www.hackerrank.com/challenges/np-inner-and-outer/problem
+
+import numpy
+
+if __name__ == '__main__':
+    A = numpy.array(input().split(), dtype=int)
+    B = numpy.array(input().split(), dtype=int)
+    print(numpy.inner(A, B))
+    print(numpy.outer(A, B))
+
