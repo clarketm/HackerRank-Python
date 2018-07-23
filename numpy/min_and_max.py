28fe83ca9578f03d9c390c01120398ceb38ba221
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 14:26:36 -0700

    add numpy -> min_and_max solution

---
+++ numpy/min_and_max.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Min and Max
+# https://www.hackerrank.com/challenges/np-min-and-max/problem
+
+import numpy
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    arr = numpy.array([input().split() for _ in range(N)], dtype=int)
+    print(numpy.max(numpy.min(arr, axis=1)))
