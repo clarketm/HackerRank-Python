fbf79265257abbc24e3aa7fe95e5d9b63610d634
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 14:56:50 -0700

    add numpy -> transpose_and_flatten solution

---
+++ numpy/transpose_and_flatten.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Transpose and Flatten
+# https://www.hackerrank.com/challenges/np-transpose-and-flatten/problem
+
+import numpy
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    arr = numpy.array([input().split() for _ in range(N)], dtype=int)
+    print(numpy.transpose(arr))
+    print(arr.flatten())
