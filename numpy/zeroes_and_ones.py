48787dd9983c6690df28c979363a1ddf10d1bc1f
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-23 13:34:56 -0700

    add numpy -> zeroes_and_ones solution

---
+++ numpy/zeroes_and_ones.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Zeroes and Ones
+# https://www.hackerrank.com/challenges/np-zeros-and-ones/problem
+
+import numpy
+
+if __name__ == '__main__':
+    dims = list(map(int, input().split()))
+    zeroes = numpy.zeros(dims, dtype=numpy.int)
+    ones = numpy.ones(dims, dtype=numpy.int)
+    print(zeroes, ones, sep='\n')
