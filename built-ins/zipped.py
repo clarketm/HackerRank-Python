d36e90bdf00e82ca0c7ecb242b067c280fb2ee58
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-23 19:34:18 +0530

    Update zipped.py

--- built-ins/zipped.py
+++ built-ins/zipped.py
@@ -1,14 +1,1 @@
-#!/usr/bin/env python3
-
-# Zipped!
-# https://www.hackerrank.com/challenges/zipped/problem
-
-if __name__ == "__main__":
-    N, X = map(int, input().split())
-    grades_by_subject = []
-
-    for _ in range(X):
-        grades_by_subject.append(map(float, input().split()))
-
-    for grades_by_student in zip(*grades_by_subject):
-        print(sum(grades_by_student) / len(grades_by_student))
+[print(sum(i) / len(i) ) for i in zip( *[map(float, input().split()) for _ in range(int(input().split()[1])) ] ) ]
